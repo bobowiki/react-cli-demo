286a2d0591567f70bce0f87d377c9b160f3da441
bobowiki <15270421125@163.com>
2024-06-12 01:25:33 +0800

    add tailwindcss

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <div className="m-3 p-3 border">
+        <h1 className="text-blue-500">Hello World</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
